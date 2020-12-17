6d0fd74af8e265c6f712435db589df604ceb118b
Kenaz <kenaz@puppetlabs.com>
2020-12-16 16:04:32 -0800

    Dumping payload

--- triggers/eventbridge-trigger/handler.py
+++ triggers/eventbridge-trigger/handler.py
@@ -5,13 +5,18 @@
 from relay_sdk import Interface, WebhookServer
 from quart import Quart, request, jsonify, make_response
 import json
+import logging
 
 relay = Interface()
 app = Quart('my-app')
 
+logging.getLogger().setLevel(logging.INFO)
+
 @app.route('/', methods=['POST'])
 async def handler():
     payload = await request.get_json()
+    logging.info("Received the following webhook payload: \n%s", json.dumps(payload, indent=4))
+
     if payload is None:
         return {'message': 'not a valid webhook'}, 400, {}
 
